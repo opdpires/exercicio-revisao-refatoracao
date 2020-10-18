b49dbb9cebab42b69cd64101e28113c54f22a851
Pedro Pires <60989202+opdpires@users.noreply.github.com>
2020-10-18 14:26:28 -0300

    Update Vendedor.cpp
    Agora este arquivo apenas contem definições dos métodos declarados no header file desta classe

--- Vendedor.cpp
+++ Vendedor.cpp
@@ -1,14 +1,28 @@
-#include <string>
-#include "Empregado.hpp"
+#include "Vendedor.hpp"
 
-class Vendedor : public Empregado {
+Vendedor::Vendedor():
+  Empregado(), quotaMensalVendas(0) {}
 
-  public:
-    std::string nome;  	  
-	
-	double quotaTotalAnual() {
-	  return quotaMensalVendas * 12;
-	}
-	
-};
+Vendedor::Vendedor(std::string _nome, double salarioH, double quotaMensal):
+  Empregado(_nome, salarioH), quotaMensalVendas(quotaMensal) {}
+  
+Vendedor::~Vendedor() {}
 
+double Vendedor::quotaTotalAnual() {
+	return quotaMensalVendas * 12;
+}
+
+void Vendedor::mostrarDadosMensais(double horas) {
+  std::cout << "Nome: " << nome << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(8) << std::endl;  
+  std::cout << "Quota vendas: " << quotaTotalAnual() << std::endl; 
+  std::cout << std::endl;
+  }
+  
+void Vendedor::setQuotaMensalVendas(double quotaMensal){
+    quotaMensalVendas = quotaMensal;
+}
+    
+double Vendedor::getQuotaMensalVendas() {
+    return quotaMensalVendas;
+}
