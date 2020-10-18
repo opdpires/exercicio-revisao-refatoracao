47bebcec64324fd1e000ba7981387d6ad4064eba
Pedro Pires <60989202+opdpires@users.noreply.github.com>
2020-10-18 14:28:11 -0300

    Update Engenheiro.cpp
    Agora este arquivo apenas contem definições dos métodos declarados no header file desta classe

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,24 @@
-#include <string>
-#include "Empregado.hpp"
+#include "Engenheiro.hpp"
 
-class Engenheiro : public Empregado {
+Engenheiro::Engenheiro():
+  Empregado(), projetos(0) {}
 
-  public:
-    std::string nome;  
-	int projetos;
-	
-};
+Engenheiro::Engenheiro(std::string _nome, double salarioH, int _projetos):
+  Empregado(_nome, salarioH), projetos(_projetos) {}
+  
+Engenheiro::~Engenheiro() {}
 
+void Engenheiro::mostrarDadosMensais (double horas) {
+  std::cout << "Nome: " << nome << std::endl;
+  std::cout << "Salario Mes: " << pagamentoMes(horas) << std::endl;
+  std::cout << "Projetos: " << projetos << std::endl;  
+  std::cout << std::endl;
+}
+
+void Engenheiro::setProjetos(int _projetos) {
+    projetos = _projetos;
+}
+    
+int Engenheiro::getProjetos() {
+    return projetos;
+}
